e1af489ca2797d29f334bfd6c50522f5829e7527
Cam Zhou <cam.zhou@grabtaxi.com>
2020-03-16 10:30:32 -0700

    init

---
+++ CardsAgainstHumanity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardsAgainstHumanity
+//
+//  Created by Cam Zhou on 3/15/20.
+//  Copyright © 2020 Cam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '13.0'
+
+target 'CardsAgainstHumanity' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CardsAgainstHumanity
+  pod 'Alamofire', '~> 5.0'
+  pod "Koloda"
+
+end
